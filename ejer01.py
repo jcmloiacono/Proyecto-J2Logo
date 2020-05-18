578c18c0cee00f927a8d1b9c936e0ea4ebcbd4ef
jcmloiacono <60507822+jcmloiacono@users.noreply.github.com>
2020-05-19 00:00:35 +0200

    Add files via upload

---
+++ ejer01.py
@@ -0,0 +1,41 @@
+
+def carga_keywords():
+    keywords = []
+    
+    with open('/Users/jean/Documents/GitHub/Proyectos Personales y Practicas/Proyecto J2Logo/keywords.txt', 'r') as file:
+        
+        for line in file:
+            if line not in keywords:
+                keywords.append(line[:len(line)-1])
+    
+    return keywords           
+
+# El usuario debera entroducir valores 0,1 ò 2
+option = int(input("Seleccione una Opcion:\n[1] – Importar palabras clave\n[2] – Mostrar palabras clave\n[0] – Salir\n"))
+
+# Se verifica si el usuario no ha introducido una opcion correcta
+i=True
+while i == True:
+    if option >=0 and option <=2:
+        i = False
+    else:
+        print("Ha introducido una Opcion Incorrecta (1,2 ò 0\n)")
+        option = int(input("Seleccione una Opcion:\n[1] – Importar palabras clave\n[2] – Mostrar palabras clave\n[0] – Salir\n"))
+
+if option == 1:
+    keywords = carga_keywords()
+    print (keywords)
+elif option == 2:
+    keywords = carga_keywords()
+    if (len(keywords)) > 20:
+        cont = 0
+        for i, j in enumerate(keywords,1):
+            if cont <= 19:
+                print (i, j)
+                cont +=1
+            else:
+                stop = input ("Presione cualquier tecla para continuar")
+                cont = 0
+    else:
+        for i, j in enumerate(keywords,1):
+            print (i, j)
